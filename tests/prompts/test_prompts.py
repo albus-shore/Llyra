5aa978684f1b020bd86e3ca726322ac9c275f0c3
albus-shore <dev@albus-shore.net>
2025-04-17 15:32:56 +0800

    Change: Modify prompts module test code and files.

--- tests/prompts/test_prompts.py
+++ tests/prompts/test_prompts.py
@@ -12,6 +12,13 @@ def test_class_initialize(prompt):
     assert prompt.call_input == None
     assert prompt.call_output == None
 
+## =========================== Config Method Test =========================== ##
+def test_config_method(prompt):
+    '''Test whether the method can set prompt config parameters properly.'''
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'})
+    assert prompt.begin == '<|begin_of_sentence|>'
+    assert prompt.end == '<|end_of_sentence|>'
+
 ## ============================= Set Method Test ============================= ##
 def test_load_method(prompt):
     '''Test whether the method can set prompt parameter attributes properly.'''
@@ -22,6 +29,7 @@ def test_load_method(prompt):
 ## ========================== Generate Methods Test ========================== ##
 def test_call_method(prompt):
     '''Test whether the method can make prompt for single inference prrperly.'''
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'})
     prompt.set({'input':'<|User|>','output':'<|Assistant|>'})
     output = prompt.call('Evening!')
-    assert output == '<|User|>Evening!<|Assistant|>'
+    assert output == '<|begin_of_sentence|><|User|>Evening!<|Assistant|><|end_of_sentence|>'
