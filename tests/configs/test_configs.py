d7ce32daaf5d6de62f7f7267e7ffddd8a509569c
albus-shore <albus_shore@outlook.com>
2025-04-17 15:02:50 +0800

    Change: Modify configs module test code and files.

--- tests/configs/test_configs.py
+++ tests/configs/test_configs.py
@@ -12,32 +12,22 @@ def config():
 def test_load_default_config_file(config):
     '''Test whether method load config file from default path properly.'''
     config.load()
-    attributs = ('model',
-                'directory',
-                'strategy',
-                'gpu',
-                'ram',
-                'path',)
-    assert config.attributes == attributs
     assert config.model == 'model'
     assert config.directory == 'models/'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == False
     assert config.path == 'models/model.gguf'
-    assert config.ram is None
+    assert config.ram == False
 
 def test_load_config_file_from_path(config):
     '''Test whether method load config form provided path properly.'''
     config.load('tests/configs/config_normal.json')
-    attributs = ('model',
-                'directory',
-                'strategy',
-                'gpu',
-                'ram',
-                'path',)
-    assert config.attributes == attributs
     assert config.model == 'model'
     assert config.directory == 'models/'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == False
     assert config.path == 'models/model.gguf'
@@ -53,6 +43,18 @@ def test_load_config_file_without_directory_key(config):
     with pytest.raises(IndexError,match='Error: Missing model file directory parameter.'):
         config.load('tests/configs/config_missing_directory.json')
 
+def test_load_config_file_without_indicate_key(config):
+    '''Test whether method load config without whole indicate key properly'''
+    config.load('tests/configs/config_missing_indicate.json')
+    assert config.model == 'model'
+    assert config.directory == 'models/'
+    assert config.indicate['begin'] == None
+    assert config.indicate['end'] == None
+    assert config.strategy == 'config/strategy.json'
+    assert config.gpu == False
+    assert config.path == 'models/model.gguf'
+    assert config.ram is None
+
 def test_load_config_file_without_strategy_key(config):
     '''Test whether method show warning properly when missing strategy key.'''
     warning = 'Warning: Missing inference strategy file.\n'
@@ -66,13 +68,15 @@ def test_update_model_config_parameter(config):
     '''Test whether method update model config parameter properly.'''
     config.load('tests/configs/config_normal.json')
     for possible in ['test','test.gguf']:
-        config.update(model=possible,
-                      directory=None,
+        config.update(model=possible,directory=None,
+                      begin=None,end=None,
                       strategy=None,
                       gpu=None,
                       ram=None,)
         assert config.model == 'test'
         assert config.path == 'models/test.gguf'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.directory == 'models/'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == False
@@ -82,16 +86,35 @@ def test_update_directory_config_parameter(config):
     '''Test whether method update directory config parameter properly.'''
     config.load('tests/configs/config_normal.json')
     for possible in ['tests','tests/']:
-        config.update(model=None,
-                      directory=possible,
+        config.update(model=None,directory=possible,
+                      begin=None,end=None,
                       strategy=None,
                       gpu=None,
                       ram=None,)
         assert config.directory == 'tests/'
         assert config.path == 'tests/model.gguf'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.model == 'model'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == False
+    assert config.ram is None
+
+def test_update_indicate_config_parameter(config):
+    '''Test whether method update indicate config parameter properly'''
+    config.load('tests/configs/config_normal.json')
+    config.update(model=None,directory=None,
+                  begin='bos',end='eos',
+                  strategy=None,
+                  gpu=None,
+                  ram=None,)
+    assert config.model == 'model'
+    assert config.directory == 'models/'
+    assert config.indicate['begin'] == 'bos'
+    assert config.indicate['end'] == 'eos'
+    assert config.strategy == 'config/strategy.json'
+    assert config.gpu == False
+    assert config.path == 'models/model.gguf'
     assert config.ram is None
 
 def test_update_strategy_config_parameter(config):
@@ -99,13 +122,15 @@ def test_update_strategy_config_parameter(config):
     And show warning when setting strategy to empty properly.
     '''
     config.load('tests/configs/config_normal.json')
-    config.update(model=None,
-                  directory=None,
+    config.update(model=None,directory=None,
+                  begin=None,end=None,
                   strategy='tests/config/strategy.json',
                   gpu=None,
                   ram=None,)
     assert config.model == 'model'
     assert config.directory == 'models/'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.strategy == 'tests/config/strategy.json'
     assert config.gpu == False
     assert config.path == 'models/model.gguf'
@@ -113,13 +138,15 @@ def test_update_strategy_config_parameter(config):
     warning = 'Warning: Missing inference strategy file.\n'
     warning += '\t\t Inference unavailiable without manual updating.'
     with pytest.warns(UserWarning,match=warning):
-        config.update(model=None,
-                  directory=None,
-                  strategy='',
-                  gpu=None,
-                  ram=None,)
+        config.update(model=None,directory=None,
+                      begin=None,end=None,
+                      strategy='',
+                      gpu=None,
+                      ram=None,)
         assert config.model == 'model'
         assert config.directory == 'models/'
+        assert config.indicate['begin'] == '<|begin_of_sentence|>'
+        assert config.indicate['end'] == '<|end_of_sentence|>'
         assert config.strategy == ''
         assert config.gpu == False
         assert config.path == 'models/model.gguf'
@@ -129,13 +156,15 @@ def test_update_strategy_config_parameter(config):
 def test_update_gpu_config_parameter(config):
     '''Test whether method update gpu config parameter properly.'''
     config.load('tests/configs/config_normal.json')
-    config.update(model=None,
-                  directory=None,
+    config.update(model=None,directory=None,
+                  begin=None,end=None,
                   strategy=None,
                   gpu=True,
                   ram=None,)
     assert config.model == 'model'
     assert config.directory == 'models/'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == True
     assert config.path == 'models/model.gguf'
@@ -144,13 +173,15 @@ def test_update_gpu_config_parameter(config):
 def test_update_ram_config_parameter(config):
     '''Test whether method update ram config parameter properly.'''
     config.load('tests/configs/config_normal.json')
-    config.update(model=None,
-                  directory=None,
+    config.update(model=None,directory=None,
+                  begin=None,end=None,
                   strategy=None,
                   gpu=None,
                   ram=False,)
     assert config.model == 'model'
     assert config.directory == 'models/'
+    assert config.indicate['begin'] == '<|begin_of_sentence|>'
+    assert config.indicate['end'] == '<|end_of_sentence|>'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == False
     assert config.path == 'models/model.gguf'
@@ -160,8 +191,8 @@ def test_update_ram_config_parameter(config):
 def test_writing_current_config_into_file_without_conflict(config):
     '''Test whether method write current config into file properly.'''
     config.load('tests/configs/config_normal.json')
-    config.update(model='test',
-                  directory='test/',
+    config.update(model='test',directory='test/',
+                  begin='bos',end='eos',
                   strategy='tests/config/strategy.json',
                   gpu=True,
                   ram=False,)
@@ -172,6 +203,7 @@ def test_writing_current_config_into_file_without_conflict(config):
             assert mock_write.called
             assert '"model": "test"' in content
             assert '"directory": "test/"' in content
+            assert '"indicate": {"begin": "bos", "end": "eos"}' in content
             assert '"strategy": "tests/config/strategy.json"' in content
             assert '"gpu": true' in content
             assert '"ram": false' in content
@@ -179,8 +211,8 @@ def test_writing_current_config_into_file_without_conflict(config):
 def test_writing_current_config_into_file_with_conflict(config):
     '''Test whether methed handle condition that meets file name conflict.'''
     config.load('tests/configs/config_normal.json')
-    config.update(model='test',
-                  directory='test/',
+    config.update(model='test',directory='test/',
+                  begin='bos',end='eos',
                   strategy='tests/config/strategy.json',
                   gpu=True,
                   ram=False,)
@@ -192,6 +224,7 @@ def test_writing_current_config_into_file_with_conflict(config):
                 assert mock_write.called
                 assert '"model": "test"' in content
                 assert '"directory": "test/"' in content
+                assert '"indicate": {"begin": "bos", "end": "eos"}' in content
                 assert '"strategy": "tests/config/strategy.json"' in content
                 assert '"gpu": true' in content
                 assert '"ram": false' in content
