32b8760d0d9e048b843915958bdf6a8c0005a178
albus-shore <dev@albus-shore.net>
2025-04-20 20:31:02 +0800

    Change: Refactor logs module for supporting iterative chat inference log recording.

--- llyra/local/logs.py
+++ llyra/local/logs.py
@@ -1,4 +1,38 @@
 import time
+### =============================== Inside Functions =============================== ###
+def new(id:int,
+        model:str,
+        prompt:str,
+        role:dict,
+        temperature:float,
+        strategy:str,
+        ) -> dict:
+    '''The function is defined for make new chat history record dictionary.
+    Args:
+        id: A integrate indicate the identity of current chat log.
+        model: A string indicate the name of model file.
+        prompt: A string indicate the content of system prompt.
+        role: A dictionary indicate input and output role of
+                iterative chat inference.
+        temperature: A float indicate the model inference temperature.
+        strategy: A string indicate the path to the strategy file.
+    Returns:
+        history: A dictionary indicate the basic information of 
+            the chat iteration.
+    '''
+    history = {
+        'id': id,
+        'type': 'chat',
+        'create_at': time.time(),
+        'model': model,
+        'prompt': prompt,
+        'role': role,
+        'iteration':[],
+        'temperature': temperature,
+        'strategy': strategy,
+            }
+    return history
+
 
 ### =============================== Expose Class =============================== ###
 class Log:
@@ -21,11 +55,11 @@ class Log:
              strategy:str) -> None:
         '''The method is defined for record log for single call inference.
         Args:
-            model: A string indicate the name of model file
-            input: A string indicate input content for model inference.
-            output: A string indicate response of model inference.
+            model: A string indicate the name of model file.
             role: A dictionary indicate input and output role of 
                 single call inference.
+            input: A string indicate input content for model inference.
+            output: A string indicate response of model inference.
             temperature: A float indicate the model inference temperature.
             strategy: A string indicate the path to the strategy file.
         '''
@@ -34,6 +68,7 @@ class Log:
         # Make history content of the inference
         history = {
             'id': self.id,
+            'type': 'call',
             'create_at': time.time(),
             'model': model,
             'role': role,
@@ -47,3 +82,55 @@ class Log:
         # Append history attribute
         self.history.append(history)
     
+    def chat(self,model:str,
+             prompt:str,
+             role:dict,
+             input:str,output:str,
+             temperature:float,
+             strategy:str,
+             keep:bool) -> None:
+        '''The method is defined for record log for iterative chat inference.
+        Args:
+            model: A string indicate the name of model file.
+            prompt: A string indicate the content of system prompt.
+            role: A dictionary indicate input and output role of
+                iterative chat inference.
+            input: A string indicate input content for model inference.
+            output: A string indicate response of model inference.
+            temperature: A float indicate the model inference temperature.
+            strategy: A string indicate the path to the strategy file.
+            keep: A boolean indicate whether continue the iteration.
+        '''
+        # Discriminate whether continue the iteration
+        if keep and self.history:
+            if self.history[-1].get('type',None) == 'chat':
+                history = self.history.pop(-1)
+            else:
+                # Update history ID
+                self.id += 1
+                # Make history content of the inference
+                history = new(id=self.id,
+                              model=model,
+                              prompt=prompt,
+                              role=role,
+                              temperature=temperature,
+                              strategy=strategy)
+        else:
+            # Update history ID
+            self.id += 1
+            # Make history content of the inference
+            history = new(id=self.id,
+                          model=model,
+                          prompt=prompt,
+                          role=role,
+                          temperature=temperature,
+                          strategy=strategy)
+        # Make iteration content
+        iteration = {
+            'query': input,
+            'response': output,
+            }
+        # Append history intertion
+        history['iteration'].append(iteration)
+        # Append history attribute
+        self.history.append(history)
--- tests/logs/test_logs.py
+++ tests/logs/test_logs.py
@@ -25,12 +25,178 @@ def test_call_method(log):
     assert log.history[0] == {
         'id': 1,
         'create_at': log.history[0]['create_at'],
+        'type': 'call',
         'model': 'model',
         'role': {'input':'user','output':'assistant'},
         'iteration': [{
-        'query': 'hello, there!',
-        'response': 'hello, how can I assist you today?'
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
             }],
         'temperature': 0.6,
         'strategy': None
         }
+    
+def test_chat_method(log):
+    '''Test whether the method can record inference history properly.'''
+    log.chat(model='model',
+             prompt='You are a kind assistant.',
+             role={'input':'user','output':'assistant'},
+             input='hello, there!',output='hello, how can I assist you today?',
+             temperature=0.6,
+             strategy=None,
+             keep=True)
+    assert log.id == 1
+    assert log.history[0] == {
+        'id': 1,
+        'create_at': log.history[0]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    log.chat(model='model',
+             prompt='You are a kind assistant.',
+             role={'input':'user','output':'assistant'},
+             input='Evening!',output='Evening!',
+             temperature=0.6,
+             strategy=None,
+             keep=True)
+    assert log.id == 1
+    assert log.history[0] == {
+        'id': 1,
+        'create_at': log.history[0]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            },
+            {
+            'query': 'Evening!',
+            'response': 'Evening!'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    log.chat(model='model',
+             prompt='You are a kind assistant.',
+             role={'input':'user','output':'assistant'},
+             input='hello, there!',output='hello, how can I assist you today?',
+             temperature=0.6,
+             strategy=None,
+             keep=False)
+    assert log.id == 2
+    assert log.history[0] == {
+        'id': 1,
+        'create_at': log.history[0]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            },
+            {
+            'query': 'Evening!',
+            'response': 'Evening!'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    assert log.history[1] == {
+        'id': 2,
+        'create_at': log.history[1]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    log.call(model='model',
+             role={'input':'user','output':'assistant'},
+             input='hello, there!',
+             output='hello, how can I assist you today?',
+             temperature=0.6,
+             strategy=None)
+    log.chat(model='model',
+             prompt='You are a kind assistant.',
+             role={'input':'user','output':'assistant'},
+             input='Evening!',output='Evening!',
+             temperature=0.6,
+             strategy=None,
+             keep=True)
+    assert log.id == 4
+    assert log.history[0] == {
+        'id': 1,
+        'create_at': log.history[0]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            },
+            {
+            'query': 'Evening!',
+            'response': 'Evening!'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    assert log.history[1] == {
+        'id': 2,
+        'create_at': log.history[1]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    assert log.history[2] == {
+        'id': 3,
+        'create_at': log.history[2]['create_at'],
+        'type': 'call',
+        'model': 'model',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'hello, there!',
+            'response': 'hello, how can I assist you today?'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    assert log.history[3] == {
+        'id': 4,
+        'create_at': log.history[3]['create_at'],
+        'type': 'chat',
+        'model': 'model',
+        'prompt': 'You are a kind assistant.',
+        'role': {'input':'user','output':'assistant'},
+        'iteration': [{
+            'query': 'Evening!',
+            'response': 'Evening!'
+            }],
+        'temperature': 0.6,
+        'strategy': None
+        }
+    
