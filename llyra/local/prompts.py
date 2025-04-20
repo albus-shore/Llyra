ea2ee1a2c1186555b1892ac3e12e2b3d0d997172
albus-shore <dev@albus-shore.net>
2025-04-20 17:01:20 +0800

    Change: Refactoe prompt module for supporting iterative chat inference prompt generation.

--- llyra/local/prompts.py
+++ llyra/local/prompts.py
@@ -10,47 +10,128 @@ class Prompt():
         # Initialize model inference indication attributes
         self.begin:str = None
         self.end:str = None
-        # Initialize single call prompt build parameter attributes
-        self.call_input:str = None
-        self.call_output:str = None
+        # Initialize prompt content placeholder attributes
+        self.history:str = None
+        self.rag:str = None
+        self.tool:str = None
+        # Initialize chat prompt build parameter attributes
+        self.iteration:str = ''
+
     ## ============================ Config Method ============================ ##
-    def config(self,indicate:dict) -> None:
+    def config(self,indicate:dict,placeholder:dict) -> None:
         '''The method is defined for set prompt config parameters with input.
         Args:
             indicate: A dictionary indicate begin and end of content 
                 indicate token for model inference.
+            placeholder: A dictionary indicate place of extra content 
+                in chat prompt.
         '''
         # Set model inference indication attributes
         self.begin = indicate['begin']
         self.end = indicate['end']
-
-    ## ============================= Set Method ============================= ##
-    def set(self,call:dict) -> None:
-        '''The method is defined for set prompt parameters with input.
-        Args:
-            call: A dictionary indicate input and output role of 
-                single call inference.
-        '''
-        # Set single call prompt parameters
-        self.call_input = call['input']
-        self.call_output = call['output']
+        # Set prompt content placeholder attributes
+        self.history = placeholder['history']
+        self.rag = placeholder['rag']
+        self.tool = placeholder['tool']
 
     ## ========================== Generate Methods ========================== ##
-    def call(self,content:str) -> str:
+    def call(self,role:dict,content:str) -> str:
         '''The method is defined for generate prompt of single call inference.
         Args: 
+            role: A dictionary indicate input and output role of 
+                single call inference.
             content: A string indicate the input content for model inference.
         Returns:
             prompt: A string indicate proper structed content for inference.            
         '''
+        # Set single call prompt parameters
+        input = role['input']
+        output = role['output']
         # Make structed prompt
-        if self.call_input or self.call_output:
+        if input or output:
             prompt = self.begin or ''
-            prompt += self.call_input or ''
+            prompt += input or ''
             prompt += content 
-            prompt += self.call_output or ''
+            prompt += output or ''
             prompt += self.end or ''
         else:
             prompt = content
-        # Return prompte for inference
+        # Return prompt for inference
         return prompt
+    
+    def chat(self,role:dict,content:str,
+             system:str) -> str:
+        '''The method is defined for generate prompt of iterative chat inference.
+        Args:
+            role: A dictionary indicate input and output role of 
+                iterative chat inference.
+            content: A string indicate the input content for model inference.
+            system: A string indicate the system prompt for model inference.
+        Returns:
+            prompt: A string indicate proper structed content for inference.
+        '''
+        # Set iterative chat current prompt parameters
+        input = role['input']
+        output = role['output']
+        # Make system prompt
+        system_prompt = ''
+        if system:
+            history_insert = False
+            prompts = system.splitlines(keepends=True)
+            for split_prompt in prompts:
+                match split_prompt.strip():
+                    case self.history:
+                        system_prompt += self.iteration + '\n'
+                        history_insert = True
+                    case self.rag:
+                        pass
+                    case self.tool:
+                        pass
+                    case _:
+                        system_prompt += split_prompt
+            if not history_insert:
+                system_prompt = self.iteration + '\n' + system_prompt
+        else:
+            system_prompt += self.iteration
+        # Make user prompt
+        if input:
+            user_prompt = input + '\n'
+        else:
+            user_prompt = ''
+        user_prompt += content + '\n'
+        user_prompt += output or ''
+        # Make structed prompt
+        if system_prompt:
+            prompt = self.begin + '\n' + system_prompt + '\n' +self.end + '\n'
+        else:
+            prompt = ''
+        prompt += self.begin + '\n' + user_prompt + '\n' + self.end
+        # Return prompt for inference
+        return prompt
+    
+    ## ===================== Additional Methods for Chat ===================== ##
+    def iterate(self,role:str,iteration:str,keep:bool) -> None:
+        '''The method is defined for manage iteration history for chat inference.
+        Args:
+            role: A string indicate the role of the iteration content.
+            iteration: A string indicate the previous iteration history.
+            keep: A boolean indicate whether continue the iteration.
+        '''
+        # Discriminate whether continue the iteration
+        if not keep:
+            self.iteration:str = ''
+        # Make history content
+        if role and self.iteration:
+            content = '\n' + role
+        elif role and not self.iteration:
+            content = role
+        else:
+            content = ''
+        if iteration and content:
+            content += '\n' + iteration
+        elif iteration and not content:
+            content += iteration
+        else:
+            content += ''
+        # Append iteration history
+        self.iteration += content
--- tests/prompts/test_prompts.py
+++ tests/prompts/test_prompts.py
@@ -9,31 +9,103 @@ def prompt():
 ## ========================== Initialize Method Test ========================== ##
 def test_class_initialize(prompt):
     '''Test whether the class can be initialized properly.'''
-    assert prompt.call_input == None
-    assert prompt.call_output == None
+    assert prompt.begin == None
+    assert prompt.end == None
+    assert prompt.history == None
+    assert prompt.rag == None
+    assert prompt.tool == None
+    assert prompt.iteration == ''
 
 ## =========================== Config Method Test =========================== ##
 def test_config_method(prompt):
     '''Test whether the method can set prompt config parameters properly.'''
-    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'})
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': '<<|History|>>','rag': '<<|RAG|>>','tool':'<<|Tool|>>'})
     assert prompt.begin == '<|begin_of_sentence|>'
     assert prompt.end == '<|end_of_sentence|>'
-
-## ============================= Set Method Test ============================= ##
-def test_load_method(prompt):
-    '''Test whether the method can set prompt parameter attributes properly.'''
-    prompt.set({'input':'user','output':'assistant'})
-    assert prompt.call_input == 'user'
-    assert prompt.call_output == 'assistant'
+    assert prompt.history == '<<|History|>>'
+    assert prompt.rag == '<<|RAG|>>'
+    assert prompt.tool == '<<|Tool|>>'
+    assert prompt.iteration == ''
 
 ## ========================== Generate Methods Test ========================== ##
 def test_call_method(prompt):
-    '''Test whether the method can make prompt for single inference prrperly.'''
-    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'})
-    prompt.set({'input':'<|User|>','output':'<|Assistant|>'})
-    output = prompt.call('Evening!')
+    '''Test whether the method can make prompt for single inference properly.'''
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': None,'rag': None,'tool':None})
+    output = prompt.call({'input': '<|User|>', 'output': '<|Assistant|>'},'Evening!')
     assert output == '<|begin_of_sentence|><|User|>Evening!<|Assistant|><|end_of_sentence|>'
-    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'})
-    prompt.set({'input':'','output':''})
-    output = prompt.call('Evening!')
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': None,'rag': None,'tool':None})
+    output = prompt.call({'input': '', 'output': '<|Assistant|>'},'Evening!')
+    assert output == '<|begin_of_sentence|>Evening!<|Assistant|><|end_of_sentence|>'
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': None,'rag': None,'tool':None})
+    output = prompt.call({'input': '', 'output': ''},'Evening!')
     assert output == 'Evening!'
+
+def test_chat_method_without_iteration(prompt):
+    # No prompt and iteration history
+    '''Test whether the method can make prompt for iterative chat inference properly.'''
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': '<<|History|>>','rag': '<<|RAG|>>','tool':'<<|Tool|>>'})
+    output = prompt.chat({'input': '<|User|>', 'output': '<|Assistant|>'},'Evening!',None)
+    assert output == '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\n<|end_of_sentence|>'
+    # Only with prompt
+    system_prompt = 'You are a kind assistant.'
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': '<<|History|>>','rag': '<<|RAG|>>','tool':'<<|Tool|>>'})
+    output = prompt.chat({'input': '<|User|>', 'output': '<|Assistant|>'},'Evening!',system_prompt)
+    expectation = '<|begin_of_sentence|>\n\nYou are a kind assistant.\n<|end_of_sentence|>\n'
+    expectation += '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\n<|end_of_sentence|>'
+    assert output == expectation
+    # Only with inference history
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': '<<|History|>>','rag': '<<|RAG|>>','tool':'<<|Tool|>>'})
+    prompt.iterate('<|User|>','Evening!',False)
+    prompt.iterate('<|Assistant|>','Evening!',True)
+    output = prompt.chat({'input': '<|User|>', 'output': '<|Assistant|>'},'Evening!',None)
+    expectation = '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\nEvening!\n<|end_of_sentence|>\n'
+    expectation += '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\n<|end_of_sentence|>'
+    assert output == expectation
+    # With iteration history and prompt which has no placeholder
+    system_prompt = 'You are a kind assistant.'
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': '<<|History|>>','rag': '<<|RAG|>>','tool':'<<|Tool|>>'})
+    prompt.iterate('<|User|>','Evening!',False)
+    prompt.iterate('<|Assistant|>','Evening!',True)
+    output = prompt.chat({'input': '<|User|>', 'output': '<|Assistant|>'},'Evening!',system_prompt)
+    expectation = '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\nEvening!\n'
+    expectation += 'You are a kind assistant.\n<|end_of_sentence|>\n'
+    expectation += '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\n<|end_of_sentence|>'
+    assert output == expectation
+    # With iteration history and prompt which has placeholder
+    system_prompt = 'You are a kind assistant.\n<<|History|>>\nThere are history.'
+    prompt.config({'begin':'<|begin_of_sentence|>','end':'<|end_of_sentence|>'},
+                  {'history': '<<|History|>>','rag': '<<|RAG|>>','tool':'<<|Tool|>>'})
+    prompt.iterate('<|User|>','Evening!',False)
+    prompt.iterate('<|Assistant|>','Evening!',True)
+    output = prompt.chat({'input': '<|User|>', 'output': '<|Assistant|>'},'Evening!',system_prompt)
+    expectation = '<|begin_of_sentence|>\nYou are a kind assistant.\n'
+    expectation += '<|User|>\nEvening!\n<|Assistant|>\nEvening!\n'
+    expectation += 'There are history.\n<|end_of_sentence|>\n'
+    expectation += '<|begin_of_sentence|>\n<|User|>\nEvening!\n<|Assistant|>\n<|end_of_sentence|>'
+    assert output == expectation
+
+## ========================== Additional Mtethods for Chat Test ========================== ##
+def test_iterate_method(prompt):
+    '''Test whether the method record iteration history properly.'''
+    prompt.iterate('<|User|>','Evening!',False)
+    assert prompt.iteration == '<|User|>\nEvening!'
+    prompt.iterate(None,None,False)
+    assert prompt.iteration == ''
+    prompt.iterate('','Evening!',False)
+    assert prompt.iteration == 'Evening!'
+    prompt.iterate('<|User|>','',False)
+    assert prompt.iteration == '<|User|>'
+    prompt.iterate('<|User|>','Evening!',False)
+    prompt.iterate('<|Assistant|>','Evening!',True)
+    assert prompt.iteration == '<|User|>\nEvening!\n<|Assistant|>\nEvening!'
+    prompt.iterate('<|User|>','Evening!',False)
+    prompt.iterate('<|Assistant|>','Evening!',False)
+    assert prompt.iteration == '<|Assistant|>\nEvening!'
