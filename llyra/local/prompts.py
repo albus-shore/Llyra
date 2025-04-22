72d747eb7a896a6a5e903617c4bf7a84529f9d49
albus-shore <dev@albus-shore.net>
2025-04-22 12:03:16 +0000

    Change: Refactor prompts module for supporting iterative chat inference.

--- llyra/local/prompts.py
+++ llyra/local/prompts.py
@@ -1,3 +1,18 @@
+### ============================= Inside Functions ============================= ###
+def make(role:str,content:str) -> dict:
+    '''The function is defined for make single prompt record in chat iteration.
+    Args:
+        role: A string indicate the role of the content.
+        content: A sting indicate the content of the prompt record.
+    Returns:
+        A dictionary indicate the single prompt record in chat iteration.
+    '''
+    # Make single prompt dictionary
+    single_prompt = {role: content}
+    # Return single prompt dictionary
+    return single_prompt
+
+
 ### =============================== Expose Class =============================== ###
 class Prompt():
     '''The class is defind for generate prompt for model inference.'''
@@ -7,6 +22,8 @@ class Prompt():
         Returns:
             prompt: A object indicate prompt for inference.
         '''
+        # Initialize chat iteration attribute
+        self.iteration:list = []
 
     ## ========================== Generate Methods ========================== ##
     def call(self,role:dict,content:str) -> str:
@@ -25,3 +42,44 @@ class Prompt():
         prompt = input + content + output
         # Return prompte for inference
         return prompt
+    
+    def chat(self,role:dict,content:str,addition:str) -> list:
+        '''The method is defined for generate prompt of iterative chat inference.
+        Args:
+            role: A dictionary indicate input and prompt role of
+                iterative chat inference.
+            content: A string indicate the input content for model inference.
+            addition: A string indicate additional prompt for model inference.
+        Returns:
+            prompt: A list indicate proper structed content for chat inference.
+        '''
+        # Get iterative chat role prompt parameters
+        prompt_role = role['prompt']
+        input_role = role['input']
+        # Get iteration record
+        prompt = self.iteration[:]
+        # Discrinimate whether add additional prompt
+        if addition:
+            additional_prompt = make(prompt_role,addition)
+            prompt.append(additional_prompt)
+        # Make structed prompt
+        user_prompt = make(input_role,content)
+        prompt.append(user_prompt)
+        # Return prompt for inference
+        return prompt
+
+    ## ===================== Additional Method for Chat ===================== ##
+    def iterate(self,role:str,content:str,keep:bool) -> None:
+        '''The method is defined for update chat iteration history record.
+        Args:
+            role: A string indicate the role of the content.
+            content: A sting indicate the content of the iteration record.
+            keep: A boolean indicate whether continue current chat iterarion.
+        '''
+        # Discriminate whether continue current chat iteration.
+        if not keep:
+            self.iteration = []
+        # Make the prompt record dictionary
+        current_record = make(role=role,content=content)
+        # Append iteration record attribute
+        self.iteration.append(current_record)
