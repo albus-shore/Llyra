926bdd9aa8d35b2211cabb2a44f932a6a72e302b
albus-shore <albus_shore@outlook.com>
2025-04-22 09:57:09 +0000

    Change: Refactor prompts moduel for more clear call prompt generation.

--- llyra/local/prompts.py
+++ llyra/local/prompts.py
@@ -7,30 +7,21 @@ class Prompt():
         Returns:
             prompt: A object indicate prompt for inference.
         '''
-        # Initialize single call prompt build parameter attributes
-        self.call_input:str = None
-        self.call_output:str = None
-
-    ## ============================= Set Mothod ============================= ##
-    def set(self,call:dict) -> None:
-        '''The method is defined for set prompt parameters with input.
-        Args:
-            call: A dictionary indicate input and output role of 
-                single call inference.
-        '''
-        # Set single call prompt parameters
-        self.call_input = call['input']
-        self.call_output = call['output']
 
     ## ========================== Generate Methods ========================== ##
-    def call(self,content:str) -> str:
+    def call(self,role:dict,content:str) -> str:
         '''The method is defined for generate prompt of single call inference.
         Args: 
+            role: A dictionary indicate input and output role of 
+                single call inference.
             content: A string indicate the input content for model inference.
         Returns:
             prompt: A string indicate proper structed content for inference.            
         '''
+        # Get single call role prompt parameters
+        input = role['input'] or ''
+        output = role['output'] or ''
         # Make structed prompt
-        prompt = self.call_input + content + self.call_output
+        prompt = input + content + output
         # Return prompte for inference
         return prompt
--- tests/prompts/test_prompts.py
+++ tests/prompts/test_prompts.py
@@ -9,19 +9,10 @@ def prompt():
 ## ========================== Initialize Method Test ========================== ##
 def test_class_initialize(prompt):
     '''Test whether the class can be initialized properly.'''
-    assert prompt.call_input == None
-    assert prompt.call_output == None
-
-## ============================= Set Method Test ============================= ##
-def test_load_method(prompt):
-    '''Test whether the method can set prompt parameter attributes properly.'''
-    prompt.set({'input':'user','output':'assistant'})
-    assert prompt.call_input == 'user'
-    assert prompt.call_output == 'assistant'
 
 ## ========================== Generate Methods Test ========================== ##
 def test_call_method(prompt):
     '''Test whether the method can make prompt for single inference prrperly.'''
-    prompt.set({'input':'<|User|>','output':'<|Assistant|>'})
-    output = prompt.call('Evening!')
+    output = prompt.call({'input': '<|User|>', 'output': '<|Assistant|>'},
+                         'Evening!')
     assert output == '<|User|>Evening!<|Assistant|>'
