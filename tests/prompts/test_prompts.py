e2be78c7c37d40959fde1c379088543057938d0e
albus-shore <albus_shore@outlook.com>
2025-04-22 12:03:16 +0000

    Change: Refactor prompts module for supporting iterative chat inference.

--- tests/prompts/test_prompts.py
+++ tests/prompts/test_prompts.py
@@ -9,6 +9,7 @@ def prompt():
 ## ========================== Initialize Method Test ========================== ##
 def test_class_initialize(prompt):
     '''Test whether the class can be initialized properly.'''
+    assert prompt.iteration == []
 
 ## ========================== Generate Methods Test ========================== ##
 def test_call_method(prompt):
@@ -16,3 +17,64 @@ def test_call_method(prompt):
     output = prompt.call({'input': '<|User|>', 'output': '<|Assistant|>'},
                          'Evening!')
     assert output == '<|User|>Evening!<|Assistant|>'
+    assert prompt.iteration == []
+
+def test_chat_method_without_iteration(prompt):
+    '''Test whether the method can make prompt for iterative chat inference properly.'''
+    role = {
+        "prompt": "system",
+        "input": "user",
+        "output": "assistant"
+        }
+    content = 'hello,there!'
+    addition = 'This is for test.'
+    output = prompt.chat(role=role,content=content,addition=addition)
+    assert output == [
+        {'system': 'This is for test.'},
+        {'user': 'hello,there!'}
+        ]
+    assert prompt.iteration == []
+    output = prompt.chat(role=role,content=content,addition=None)
+    assert output == [
+        {'user': 'hello,there!'}
+        ]
+    assert prompt.iteration == []
+
+def test_chat_method_with_iteration(prompt):
+    '''
+    Test whether the method can not affect the iteration history.
+    And make promp for iterative chat inference properly.
+    '''
+    prompt.iteration.append({'user': 'Hello, there!'})
+    prompt.iteration.append({'assistant': 'Greeting, how can I assistant you today?'})
+    role = {
+        "prompt": "system",
+        "input": "user",
+        "output": "assistant"
+        }
+    content = 'hello,there!'
+    addition = 'This is for test.'
+    output = prompt.chat(role=role,content=content,addition=addition)
+    assert output == [
+        {'user': 'Hello, there!'},
+        {'assistant': 'Greeting, how can I assistant you today?'},
+        {'system': 'This is for test.'},
+        {'user': 'hello,there!'}
+        ]
+    assert prompt.iteration == [
+        {'user': 'Hello, there!'},
+        {'assistant': 'Greeting, how can I assistant you today?'},
+        ]
+
+## =========================== Iterate Method Test =========================== ##
+def test_iterate_method(prompt):
+    '''Teset whether the method can make iteration record properly.'''
+    prompt.iterate('user','Hello, there!',True)
+    assert prompt.iteration == [{'user': 'Hello, there!'}]
+    prompt.iterate('assistant','Greeting, how can I assistant you today?',True)
+    assert prompt.iteration == [
+        {'user': 'Hello, there!'},
+        {'assistant': 'Greeting, how can I assistant you today?'}
+        ]
+    prompt.iterate('user','Introduce yourself.',False)
+    assert prompt.iteration == [{'user': 'Introduce yourself.'}]
