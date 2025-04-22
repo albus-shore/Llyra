bfacd876aa64737ba7304e9012e73a36e905050c
albus-shore <albus_shore@outlook.com>
2025-04-22 05:44:43 +0000

    Change: Refactor configs module for more IDE friendly development and new config parameter loading support.

--- tests/configs/test_configs.py
+++ tests/configs/test_configs.py
@@ -8,44 +8,39 @@ def config():
     config = Config()
     return config
 
+@pytest.fixture
+def loaded_config():
+    loaded_config = Config()
+    loaded_config.load('tests/configs/config_normal.json')
+    return loaded_config
+
 ## ========================== Load Method Test ========================== ##
 def test_load_default_config_file(config):
     '''Test whether method load config file from default path properly.'''
-    config.load()
-    attributs = ('model',
-                'directory',
-                'strategy',
-                'gpu',
-                'format',
-                'ram',
-                'path',)
-    assert config.attributes == attributs
-    assert config.model == 'model'
-    assert config.directory == 'models/'
-    assert config.strategy == 'config/strategy.json'
-    assert config.gpu == False
-    assert config.format == 'llama-2'
-    assert config.path == 'models/model.gguf'
-    assert config.ram is None
+    warning = 'Warning: Missing chat format.\n'
+    warning += '\t\t Chat inference may unavailiable '
+    warning += "when chat format not contain in model's metadate "
+    warning += "or not manual updating."
+    with pytest.warns(UserWarning,match=warning):
+        config.load()
+        assert config.model == 'model'
+        assert config.directory == 'models/'
+        assert config.strategy == 'config/strategy.json'
+        assert config.gpu == False
+        assert config.format == None
+        assert config.path == 'models/model.gguf'
+        assert config.ram is False
 
 def test_load_config_file_from_path(config):
     '''Test whether method load config form provided path properly.'''
     config.load('tests/configs/config_normal.json')
-    attributs = ('model',
-                'directory',
-                'strategy',
-                'gpu',
-                'format',
-                'ram',
-                'path',)
-    assert config.attributes == attributs
     assert config.model == 'model'
     assert config.directory == 'models/'
     assert config.strategy == 'config/strategy.json'
     assert config.gpu == False
     assert config.format == 'llama-2'
     assert config.path == 'models/model.gguf'
-    assert config.ram is None
+    assert config.ram is False
 
 def test_load_config_file_without_model_key(config):
     '''Test whether method raise exception properly when missing model key.'''
@@ -63,160 +58,173 @@ def test_load_config_file_without_strategy_key(config):
     warning += '\t\t Inference unavailiable without manual updating.'
     with pytest.warns(UserWarning,match=warning):
         config.load('tests/configs/config_missing_strategy.json')
+        assert config.model == 'model'
+        assert config.directory == 'models/'
+        assert config.strategy == None
+        assert config.gpu == False
+        assert config.format == 'llama-2'
+        assert config.path == 'models/model.gguf'
+        assert config.ram is False
 
 def test_load_config_file_without_format_key(config):
     '''Test whether method show warning properly when missing strategy key.'''
-    warning = 'Warning: Missing chat format.'
-    warning += '\t\t Chat inference unavailiable without manual updating'
+    warning = 'Warning: Missing chat format.\n'
+    warning += '\t\t Chat inference may unavailiable '
+    warning += "when chat format not contain in model's metadate "
+    warning += "or not manual updating."
     with pytest.warns(UserWarning,match=warning):
         config.load('tests/configs/config_missing_format.json')
+        assert config.model == 'model'
+        assert config.directory == 'models/'
+        assert config.strategy == 'config/strategy.json'
+        assert config.gpu == False
+        assert config.format == None
+        assert config.path == 'models/model.gguf'
+        assert config.ram is False
 
 ## ============================= Update Method Test ============================= ##
-def test_update_model_config_parameter(config):
+def test_update_model_config_parameter(loaded_config):
     '''Test whether method update model config parameter properly.'''
-    config.load('tests/configs/config_normal.json')
     for possible in ['test','test.gguf']:
-        config.update(model=possible,
+        loaded_config.update(model=possible,
                       directory=None,
                       strategy=None,
                       gpu=None,
                       format=None,
                       ram=None,)
-        assert config.model == 'test'
-        assert config.path == 'models/test.gguf'
-    assert config.directory == 'models/'
-    assert config.strategy == 'config/strategy.json'
-    assert config.gpu == False
-    assert config.format == 'llama-2'
-    assert config.ram is None
+        assert loaded_config.model == 'test'
+        assert loaded_config.path == 'models/test.gguf'
+    assert loaded_config.directory == 'models/'
+    assert loaded_config.strategy == 'config/strategy.json'
+    assert loaded_config.gpu == False
+    assert loaded_config.format == 'llama-2'
+    assert loaded_config.ram == False
 
-def test_update_directory_config_parameter(config):
+def test_update_directory_config_parameter(loaded_config):
     '''Test whether method update directory config parameter properly.'''
-    config.load('tests/configs/config_normal.json')
     for possible in ['tests','tests/']:
-        config.update(model=None,
+        loaded_config.update(model=None,
                       directory=possible,
                       strategy=None,
                       gpu=None,
                       format=None,
                       ram=None,)
-        assert config.directory == 'tests/'
-        assert config.path == 'tests/model.gguf'
-    assert config.model == 'model'
-    assert config.strategy == 'config/strategy.json'
-    assert config.gpu == False
-    assert config.format == 'llama-2'
-    assert config.ram is None
+        assert loaded_config.directory == 'tests/'
+        assert loaded_config.path == 'tests/model.gguf'
+    assert loaded_config.model == 'model'
+    assert loaded_config.strategy == 'config/strategy.json'
+    assert loaded_config.gpu == False
+    assert loaded_config.format == 'llama-2'
+    assert loaded_config.ram == False
 
-def test_update_strategy_config_parameter(config):
+def test_update_strategy_config_parameter(loaded_config):
     '''Test whether method update strategy config parameter properly.
     And show warning when setting strategy to empty properly.
     '''
-    config.load('tests/configs/config_normal.json')
-    config.update(model=None,
+    loaded_config.update(model=None,
                   directory=None,
                   strategy='tests/config/strategy.json',
                   gpu=None,
                   format=None,
                   ram=None,)
-    assert config.model == 'model'
-    assert config.directory == 'models/'
-    assert config.strategy == 'tests/config/strategy.json'
-    assert config.gpu == False
-    assert config.format == 'llama-2'
-    assert config.path == 'models/model.gguf'
-    assert config.ram is None
+    assert loaded_config.model == 'model'
+    assert loaded_config.directory == 'models/'
+    assert loaded_config.strategy == 'tests/config/strategy.json'
+    assert loaded_config.gpu == False
+    assert loaded_config.format == 'llama-2'
+    assert loaded_config.path == 'models/model.gguf'
+    assert loaded_config.ram == False
     warning = 'Warning: Missing inference strategy file.\n'
     warning += '\t\t Inference unavailiable without manual updating.'
     with pytest.warns(UserWarning,match=warning):
-        config.update(model=None,
+        loaded_config.update(model=None,
                   directory=None,
                   strategy='',
                   gpu=None,
                   format=None,
                   ram=None,)
-        assert config.model == 'model'
-        assert config.directory == 'models/'
-        assert config.strategy == ''
-        assert config.gpu == False
-        assert config.format == 'llama-2'
-        assert config.path == 'models/model.gguf'
-        assert config.ram is None
+        assert loaded_config.model == 'model'
+        assert loaded_config.directory == 'models/'
+        assert loaded_config.strategy == ''
+        assert loaded_config.gpu == False
+        assert loaded_config.format == 'llama-2'
+        assert loaded_config.path == 'models/model.gguf'
+        assert loaded_config.ram == False
 
-def test_update_format_config_parameter(config):
+def test_update_format_config_parameter(loaded_config):
     '''Test whether method update format config parameter properly.
     And show warning when setting format to empty properly.
     '''
-    config.load('tests/configs/config_normal.json')
-    config.update(model=None,
+    loaded_config.load('tests/configs/config_normal.json')
+    loaded_config.update(model=None,
                   directory=None,
                   strategy=None,
                   gpu=None,
                   format='openai',
                   ram=None,)
-    assert config.model == 'model'
-    assert config.directory == 'models/'
-    assert config.strategy == 'config/strategy.json'
-    assert config.gpu == False
-    assert config.format == 'openai'
-    assert config.path == 'models/model.gguf'
-    assert config.ram is None
-    warning = 'Warning: Missing chat format.'
-    warning += '\t\t Chat inference unavailiable without manual updating'
+    assert loaded_config.model == 'model'
+    assert loaded_config.directory == 'models/'
+    assert loaded_config.strategy == 'config/strategy.json'
+    assert loaded_config.gpu == False
+    assert loaded_config.format == 'openai'
+    assert loaded_config.path == 'models/model.gguf'
+    assert loaded_config.ram == False
+    warning = 'Warning: Missing chat format.\n'
+    warning += '\t\t Chat inference may unavailiable '
+    warning += "when chat format not contain in model's metadate "
+    warning += "or not manual updating."
     with pytest.warns(UserWarning,match=warning):
-        config.update(model=None,
+        loaded_config.update(model=None,
                   directory=None,
                   strategy=None,
                   gpu=None,
                   format='',
                   ram=None,)
-        assert config.model == 'model'
-        assert config.directory == 'models/'
-        assert config.strategy == 'config/strategy.json'
-        assert config.gpu == False
-        assert config.format == ''
-        assert config.path == 'models/model.gguf'
-        assert config.ram is None
+        assert loaded_config.model == 'model'
+        assert loaded_config.directory == 'models/'
+        assert loaded_config.strategy == 'config/strategy.json'
+        assert loaded_config.gpu == False
+        assert loaded_config.format == ''
+        assert loaded_config.path == 'models/model.gguf'
+        assert loaded_config.ram == False
 
-def test_update_gpu_config_parameter(config):
+def test_update_gpu_config_parameter(loaded_config):
     '''Test whether method update gpu config parameter properly.'''
-    config.load('tests/configs/config_normal.json')
-    config.update(model=None,
+    loaded_config.update(model=None,
                   directory=None,
                   strategy=None,
                   gpu=True,
                   format=None,
                   ram=None,)
-    assert config.model == 'model'
-    assert config.directory == 'models/'
-    assert config.strategy == 'config/strategy.json'
-    assert config.gpu == True
-    assert config.format == 'llama-2'
-    assert config.path == 'models/model.gguf'
-    assert config.ram is None
+    assert loaded_config.model == 'model'
+    assert loaded_config.directory == 'models/'
+    assert loaded_config.strategy == 'config/strategy.json'
+    assert loaded_config.gpu == True
+    assert loaded_config.format == 'llama-2'
+    assert loaded_config.path == 'models/model.gguf'
+    assert loaded_config.ram == False
 
-def test_update_ram_config_parameter(config):
+def test_update_ram_config_parameter(loaded_config):
     '''Test whether method update ram config parameter properly.'''
-    config.load('tests/configs/config_normal.json')
-    config.update(model=None,
+    loaded_config.load('tests/configs/config_normal.json')
+    loaded_config.update(model=None,
                   directory=None,
                   strategy=None,
                   gpu=None,
                   format=None,
-                  ram=False,)
-    assert config.model == 'model'
-    assert config.directory == 'models/'
-    assert config.strategy == 'config/strategy.json'
-    assert config.gpu == False
-    assert config.format == 'llama-2'
-    assert config.path == 'models/model.gguf'
-    assert config.ram == False
+                  ram=True,)
+    assert loaded_config.model == 'model'
+    assert loaded_config.directory == 'models/'
+    assert loaded_config.strategy == 'config/strategy.json'
+    assert loaded_config.gpu == False
+    assert loaded_config.format == 'llama-2'
+    assert loaded_config.path == 'models/model.gguf'
+    assert loaded_config.ram == True
 
 ## ========================== Write Method Test ========================== ##
-def test_writing_current_config_into_file_without_conflict(config):
+def test_writing_current_config_into_file_without_conflict(loaded_config):
     '''Test whether method write current config into file properly.'''
-    config.load('tests/configs/config_normal.json')
-    config.update(model='test',
+    loaded_config.update(model='test',
                   directory='test/',
                   strategy='tests/config/strategy.json',
                   gpu=True,
@@ -224,7 +232,7 @@ def test_writing_current_config_into_file_without_conflict(config):
                   ram=False,)
     with patch.object(Path,'exists',return_value=False):
         with patch.object(Path,'write_text') as mock_write:
-            config.write()
+            loaded_config.write()
             content = mock_write.call_args[0][0]
             assert mock_write.called
             assert '"model": "test"' in content
@@ -234,10 +242,9 @@ def test_writing_current_config_into_file_without_conflict(config):
             assert '"format": "openai"' in content
             assert '"ram": false' in content
 
-def test_writing_current_config_into_file_with_conflict(config):
+def test_writing_current_config_into_file_with_conflict(loaded_config):
     '''Test whether methed handle condition that meets file name conflict.'''
-    config.load('tests/configs/config_normal.json')
-    config.update(model='test',
+    loaded_config.update(model='test',
                   directory='test/',
                   strategy='tests/config/strategy.json',
                   gpu=True,
@@ -246,7 +253,7 @@ def test_writing_current_config_into_file_with_conflict(config):
     with patch.object(Path,'exists',return_value=True):
         with patch('builtins.input',return_value='w'):
             with patch.object(Path,'write_text') as mock_write:
-                config.write()
+                loaded_config.write()
                 content = mock_write.call_args[0][0]
                 assert mock_write.called
                 assert '"model": "test"' in content
@@ -260,5 +267,5 @@ def test_writing_current_config_into_file_with_conflict(config):
         with patch('builtins.input',return_value='q'):
             with patch.object(Path,'write_text') as mock_write:
                 with pytest.raises(FileExistsError):
-                    config.write()
+                    loaded_config.write()
                     assert not mock_write.called
