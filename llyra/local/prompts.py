f76aeb25f5d911ff79408bc12213083c79363da4
albus-shore <dev@albus-shore.net>
2025-05-14 21:25:57 +0800

    Change: Refactor 'prompts' module in 'local' sub-package with adding 'prompts' module in 'base' sub-package for more clear struct.

--- llyra/local/prompts.py
+++ llyra/local/prompts.py
@@ -1,33 +1,44 @@
-### ============================= Inside Functions ============================= ###
-def make(role:str,content:str) -> dict:
-    '''The function is defined for make single prompt record in chat iteration.
-    Args:
-        role: A string indicate the role of the content.
-        content: A sting indicate the content of the prompt record.
-    Returns:
-        A dictionary indicate the single prompt record in chat iteration.
-    '''
-    # Make single prompt dictionary
-    single_prompt = {'role':role,
-                     'content': content}
-    # Return single prompt dictionary
-    return single_prompt
+from ..base.prompts import Prompt
 
-
-### =============================== Expose Class =============================== ###
-class Prompt():
-    '''The class is defind for generate prompt for model inference.'''
+class PromptLocal(Prompt):
+    '''The class is defind for generate prompt for local model inference.'''
+    ### ============================ Dynamic Methods ============================ ###
     ## ========================== Initialize Method ========================== ##
-    def __init__(self) -> object:
-        '''The method is defined for initialize Prompt class object.
-        Returns:
-            prompt: A object indicate prompt for inference.
-        '''
-        # Initialize chat iteration attribute
-        self.iteration:list = []
+    def __init__(self) -> None:
+        '''The method is defined for initialize PromptLocal class object.'''
+        # Initialize parent class
+        super().__init__()
 
-    ## ========================== Generate Methods ========================== ##
-    def call(self,role:dict,content:str) -> str:
+    ## =========================== Generate Method =========================== ##
+    def chat(self,role:dict,content:str,addition:str) -> list:
+        '''The method is defined for generate prompt of iterative chat inference.
+        Args:
+            role: A dictionary indicate input and prompt role of
+                iterative chat inference.
+            content: A string indicate the input content for model inference.
+            addition: A string indicate additional prompt for model inference.
+        Returns:
+            prompt: A list indicate proper structed content for chat inference.
+        '''
+        # Get iterative chat role prompt parameters
+        prompt = role['prompt']
+        input = role['input']
+        # Get iteration record
+        iteration_prompt = self._iteration[:]
+        # Discrinimate whether add additional prompt
+        if addition:
+            additional_prompt = self.make(prompt,addition)
+            iteration_prompt.insert(0,additional_prompt)
+        # Make structed prompt
+        user_prompt = self.make(input,content)
+        iteration_prompt.append(user_prompt)
+        # Return prompt for inference
+        return iteration_prompt
+    
+    ### ============================ Static Methods ============================ ###
+    ## =========================== Generate Method =========================== ##
+    @staticmethod
+    def call(role:dict,content:str) -> str:
         '''The method is defined for generate prompt of single call inference.
         Args: 
             role: A dictionary indicate input and output role of 
@@ -44,45 +55,3 @@ class Prompt():
         # Return prompte for inference
         return prompt
     
-    def chat(self,role:dict,content:str,addition:str) -> list:
-        '''The method is defined for generate prompt of iterative chat inference.
-        Args:
-            role: A dictionary indicate input and prompt role of
-                iterative chat inference.
-            content: A string indicate the input content for model inference.
-            addition: A string indicate additional prompt for model inference.
-        Returns:
-            prompt: A list indicate proper structed content for chat inference.
-        '''
-        # Get iterative chat role prompt parameters
-        prompt_role = role['prompt']
-        input_role = role['input']
-        # Get iteration record
-        prompt = self.iteration[:]
-        # Discrinimate whether add additional prompt
-        if addition:
-            additional_prompt = make(prompt_role,addition)
-            prompt.append(additional_prompt)
-        # Make structed prompt
-        user_prompt = make(input_role,content)
-        prompt.append(user_prompt)
-        # Return prompt for inference
-        return prompt
-
-    ## ===================== Additional Method for Chat ===================== ##
-    def iterate(self,role:str,content:str,keep:bool) -> None:
-        '''The method is defined for update chat iteration history record.
-        Args:
-            role: A string indicate the role of the content.
-            content: A sting indicate the content of the iteration record.
-            keep: A boolean indicate whether continue last chat iterarion.
-        '''
-        # Discriminate whether continue last chat iteration
-        if not keep:
-            self.iteration = []
-        # Discriminate whether make new iteration record
-        if role != None and content !=None:
-            # Make the prompt record dictionary
-            last_record = make(role=role,content=content)
-            # Append iteration record attribute
-            self.iteration.append(last_record)
--- tests/prompts/test_prompts_local.py
+++ tests/prompts/test_prompts_local.py
@@ -1,15 +1,10 @@
 import pytest
-from llyra.local.prompts import Prompt
+from llyra.local.prompts import PromptLocal
 
 @pytest.fixture
 def prompt():
-    prompt = Prompt()
+    prompt = PromptLocal()
     return prompt
-
-## ========================== Initialize Method Test ========================== ##
-def test_class_initialize(prompt):
-    '''Test whether the class can be initialized properly.'''
-    assert prompt.iteration == []
 
 ## ========================== Generate Methods Test ========================== ##
 def test_call_method(prompt):
@@ -17,7 +12,7 @@ def test_call_method(prompt):
     output = prompt.call({'input': '<|User|>', 'output': '<|Assistant|>'},
                          'Evening!')
     assert output == '<|User|>Evening!<|Assistant|>'
-    assert prompt.iteration == []
+    assert prompt._iteration == []
 
 def test_chat_method_without_iteration(prompt):
     '''Test whether the method can make prompt for iterative chat inference properly.'''
@@ -33,20 +28,20 @@ def test_chat_method_without_iteration(prompt):
         {'role': 'system','content': 'This is for test.'},
         {'role': 'user', 'content': 'hello,there!'}
         ]
-    assert prompt.iteration == []
+    assert prompt._iteration == []
     output = prompt.chat(role=role,content=content,addition=None)
     assert output == [
         {'role': 'user', 'content': 'hello,there!'}
         ]
-    assert prompt.iteration == []
+    assert prompt._iteration == []
 
 def test_chat_method_with_iteration(prompt):
     '''
     Test whether the method can not affect the iteration history.
     And make promp for iterative chat inference properly.
     '''
-    prompt.iteration.append({'role': 'user', 'content': 'Hello, there!'})
-    prompt.iteration.append({'role': 'assistant', 'content': 'Greeting, how can I assistant you today?'})
+    prompt._iteration.append({'role': 'user', 'content': 'Hello, there!'})
+    prompt._iteration.append({'role': 'assistant', 'content': 'Greeting, how can I assistant you today?'})
     role = {
         "prompt": "system",
         "input": "user",
@@ -56,33 +51,13 @@ def test_chat_method_with_iteration(prompt):
     addition = 'This is for test.'
     output = prompt.chat(role=role,content=content,addition=addition)
     assert output == [
+        {'role': 'system', 'content': 'This is for test.'},
         {'role': 'user', 'content': 'Hello, there!'},
         {'role': 'assistant', 'content': 'Greeting, how can I assistant you today?'},
-        {'role': 'system', 'content': 'This is for test.'},
         {'role': 'user', 'content': 'hello,there!'}
         ]
-    assert prompt.iteration == [
+    assert prompt._iteration == [
         {'role': 'user', 'content': 'Hello, there!'},
         {'role': 'assistant', 'content': 'Greeting, how can I assistant you today?'},
         ]
 
-## =========================== Iterate Method Test =========================== ##
-def test_iterate_method(prompt):
-    '''Teset whether the method can make iteration record properly.'''
-    prompt.iterate('user','Hello, there!',True)
-    assert prompt.iteration == [{'role': 'user', 'content': 'Hello, there!'}]
-    prompt.iterate('assistant','Greeting, how can I assistant you today?',True)
-    assert prompt.iteration == [
-        {'role': 'user', 'content': 'Hello, there!'},
-        {'role': 'assistant', 'content': 'Greeting, how can I assistant you today?'}
-        ]
-    prompt.iterate('user','Introduce yourself.',False)
-    assert prompt.iteration == [{'role': 'user', 'content': 'Introduce yourself.'}]
-    prompt.iterate(None,'This is for test.',True)
-    assert prompt.iteration == [{'role': 'user', 'content': 'Introduce yourself.'}]
-    prompt.iterate('user',None,True)
-    assert prompt.iteration == [{'role': 'user', 'content': 'Introduce yourself.'}]
-    prompt.iterate(None,None,True)
-    assert prompt.iteration == [{'role': 'user', 'content': 'Introduce yourself.'}]
-    prompt.iterate(None,None,False)
-    assert prompt.iteration == []
