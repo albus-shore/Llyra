8487499b1530a153028635895ca6e238d37862f9
albus-shore <dev@albus-shore.net>
2025-06-21 17:31:37 +0800

    Change: Refactor 'Config()' class for refactoring 'LocalConfig(Config)' and 'RemoteConfig(Config)' classes.

--- llyra/base/__init__.py
+++ llyra/base/__init__.py
@@ -1,4 +1,1 @@
-from .configs import Config
 from .strategys import Strategy
-from .prompts import Prompt
-from .logs import Log
---
+++ llyra/components/configs/basic.py
@@ -0,0 +1,50 @@
+import tomllib
+from pathlib import Path
+from ...errors.configs import ConfigSectionMissingError, ConfigParameterMissingError
+
+class Config:
+    '''The class is defined to define basic attributes and internal methods, 
+        for working with configurations.'''
+    ## ============================= Initialize Method ============================= ##
+    def __init__(self) -> None:
+        '''The method is defined for initializing Config class object.'''
+        # Define default path to config file
+        self.path:Path = Path('configs/config.toml')
+        # Define global config attribute
+        self.strategy:Path = None
+        # Define assistant internal attribute
+        self._content:dict = None
+
+    ## ======================== Internal Load Method ======================== ##
+    def _load(self,path:str|Path) -> dict:
+        '''The method is defined for load config from default or custom path.
+        Args:
+            path: A string indicate the custom path to the config file.
+        '''
+        # Discriminate whether loading from custom path or default path
+        if path:
+            if type(path) == Path:
+                self.path = path
+            else:
+                self.path:Path = Path(path)
+        # Load config file content
+        try:
+            with self.path.open('rb') as obj:
+                self._content = tomllib.load(obj)
+        except FileNotFoundError:
+            if path:
+                error = 'Config file not found in provided path.'
+            else:
+                error = 'Missing config file.'
+            raise FileNotFoundError(error)
+        # Read global config attribute
+        try:
+            content = self._content['global']
+        except KeyError:
+            raise ConfigSectionMissingError('global')
+        try:
+            strategy = content['strategy']
+        except KeyError:
+            raise ConfigParameterMissingError('global','strategy')
+        else:
+            self.strategy:Path = Path(strategy)
