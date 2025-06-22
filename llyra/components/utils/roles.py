237fbcffbecfb4f0e8050539a2a460f7206f75b9
albus-shore <dev@albus-shore.net>
2025-06-22 16:21:03 +0800

    Change: Fix docstring error.

--- llyra/components/utils/roles.py
+++ llyra/components/utils/roles.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Role:
-    '''This class is defined for recording role parameters.
+    '''This class is defined for managing role parameters.
     Args:
         prompt: A string indicate the role of additional prompt.
         input: A string indicate the role of input.
