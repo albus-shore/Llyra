61f3cfd8b62c591e8a040a9bc333a98c96c5d596
albus-shore <dev@albus-shore.net>
2025-07-25 14:46:33 +0800

    Change: Add `LogError` series custom errors.

--- llyra/exceptions/__init__.py
+++ llyra/exceptions/__init__.py
@@ -1,3 +1,4 @@
 from .components import ConfigError
 from .components import StrategyError
+from .components import LogError, LogSectionError, LogBranchError, LogBaseError, LogRecordError
 from .backends import RemoteError
