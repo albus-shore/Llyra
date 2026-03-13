db4c136ad68f03b62b5ca703e4203e3ed93643db
albus-shore <dev@albus-shore.net>
2026-03-13 15:13:27 +0800

    Initial Commit: Add `ConfigData` dataclass for inference configuration management.

---
+++ llyra/data/components/configs/definition.py
@@ -0,0 +1,20 @@
+from .utils.classes import LocalConfigData,RemoteConfigData
+from dataclasses import dataclass
+from pathlib import Path
+
+## ============================ `ConfigData` Dataclass ============================ ##
+@dataclass(frozen=True)
+class ConfigData:
+    '''This class is a dataclass contains information 
+    for general inference configuration.
+    Args:
+        strategy: A `Path` instance indicates the location of 
+            inference strategy `toml` file.
+        local: A `LocalConfigData` instance indicates 
+            the local inference configuration.
+        remote: A `RemoteConfigData` instance indicates 
+            the remote inference configuration.
+    '''
+    strategy: Path
+    local: LocalConfigData
+    remote: RemoteConfigData
