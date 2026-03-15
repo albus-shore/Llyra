d2fcc8d8a6b626655f8b418ae4d2d2aab87d0503
albus-shore <dev@albus-shore.net>
2026-03-15 19:09:12 +0800

    Initial Commit: Build sub-package `exceptions` replacing sub-package `errors`.

---
+++ llyra/exceptions/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+
+## ================================== Basic Error ================================== ##
+class LlyraError(Exception):
+    '''This exception is the base of all llyra related errors.'''
+    pass
