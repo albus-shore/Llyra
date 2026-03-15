3bf77450f1a0b5ef43935867a61950c3aae23ffe
albus-shore <dev@albus-shore.net>
2026-03-15 19:13:51 +0800

    Change: Refactor sub-package `utils` in sub-package `components/configs/`.

--- llyra/components/configs/utils/__init__.py
+++ llyra/components/configs/utils/__init__.py
@@ -1,5 +1,1 @@
-from .funcs import struct_path
-from .funcs import struct_suffix, struct_model_name
-from .funcs import struct_url
-from .classes import Model
-from .classes import Server
+__version__ = '0.0.1'
