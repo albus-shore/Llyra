afb9973351f3214f91a25322babf4d454312e293
albus-shore <dev@albus-shore.net>
2026-03-17 11:55:25 +0800

    Change: Refactor component `Strategy`.

--- llyra/components/strategys/definition.py
+++ llyra/components/strategys/definition.py
@@ -1,152 +1,220 @@
-from .utils import Call, Chat
-from ..utils import Role
-from ...errors.strategys import StrategySectionMissingError, StrategyParameterMissingError
+from ...data.components import StrategyData
+from ...data.components.utils.roles import Role, LlamaCppRole
+from ...data.components.startegys.untils.classes import CallStrategyData, \
+    ChatStrategyData
+from ...exceptions.components.strategys import StrategyOutOfRangeError, \
+    StrategySectionMissingError, StrategyParameterMissingError, \
+    StrategyCrashError, StrategyNoAvailableModeError
+from tomllib import load as load_toml
 from warnings import warn
+from copy import deepcopy
 from pathlib import Path
-import tomllib
+
 
 class Strategy:
-    '''The class is defined to define universal attributes and methods,
-    for working with inference strategies.'''
-    ## ============================= Initialize Method ============================= ##
-    def __init__(self) -> None:
-        '''The method is defined for initalizeing Strategy class object.'''
-        # Define necessary object attributes
-        self.call:Call = None
-        self.chat:Chat = None
-
-    ## ================================ Load Method ================================ ##
-    def load(self,path:Path) -> None:
-        '''The method is defined for loading strategy content from file.
+    '''This class is a component for inference strategy manipulation.'''
+    ## =============================== `load` Method =============================== ##
+    def load(self,path:Path) -> StrategyData:
+        '''The method will load strategy from designated `toml` file.
         Args:
-            path: A Path instance indicate the path to the strategy file.
+            path: A `Path` instance indicates the path to 
+                inference strategy `toml` file.
         '''
-        # Load strategy file
+        # Load strategy content
         try:
             with path.open('rb') as obj:
-                content = tomllib.load(obj)
+                strategy:dict = load_toml(obj)
         except FileNotFoundError:
-            raise FileNotFoundError('Strategy file not found in provided path.')
-        # Read call strategy parameters
-        ## Extract all call strategy parameters
+            raise FileNotFoundError('Strategy File Not Found.')
+        # Read call attributes
         try:
-            call = content['call']
+            call_strategy:dict = strategy['call']
         except KeyError:
-            raise StrategySectionMissingError('call')
-        ## Read call strategy parameters
-        try:
-            stop = call['stop']
-        except KeyError:
-            message = 'Missing `stop` parameter of `call` section '
-            message += "in `strategy.toml` , auto-fallback to `[]`."
-            warn(message,RuntimeWarning)
-            stop = []
-        try:
-            temperature = call['temperature']
-        except KeyError:
-            message = 'Missing `temperature` parameter of `call` section '
-            message += 'in `strategy.toml` , auto-fallback to `0`.'
-            warn(message,RuntimeWarning)
-            temperature = 0
-        self.call:Call = Call(stop,temperature)
-        # Read chat strategy parameters
-        ## Extract all chat strategy parameters
-        try:
-            chat = content['chat']
-        except KeyError:
-            raise StrategySectionMissingError('chat')
-        ## Read additonal prompt
-        try:
-            addition_path = chat['prompt']
-        except KeyError:
-            addition = None
+            # Make warning
+            message = 'Missing `call` section in strategy file.\n'\
+                '`call` inference not available.'
+            warn(message=message,category=RuntimeWarning)
+            # Make call strategy data False
+            call_strategy_data = False
         else:
-            addition_obj = Path(addition_path)
+            # Read call stop
             try:
-                addition = addition_obj.read_text(encoding='utf-8')
-            except FileNotFoundError:
-                raise FileNotFoundError('Prompt file not found in provided path.')
-        ## Read chat role parameters
-        ### Etra all chat role parameters
+                call_stop:str|list = call_strategy['stop']
+            except KeyError:
+                raise StrategyParameterMissingError(section='call',
+                                                    parameter='stop')
+            # Read call temperature
+            try:
+                call_temperature:float = call_strategy['temperature']
+            except KeyError:
+                raise StrategyParameterMissingError(section='call',
+                                                    parameter='temperature')
+            # Build `CallStrategyData` dataclass
+            call_strategy_data = CallStrategyData(stop=call_stop,
+                                                  temperature=call_temperature)
+        # Read chat attributes
         try:
-            role_content = chat['role']
+            chat_strategy:dict = strategy['chat']
         except KeyError:
-            raise StrategySectionMissingError('chat.role')
-        ### Read chat role parameters
-        try:
-            prompt = role_content['prompt']
-        except KeyError:
-            if addition:
-                raise StrategyParameterMissingError('chat.role','prompt')
+            # Make warning
+            message = 'Missing `chat` section in strategy file.\n'\
+                '`chat` inference not available.'
+            warn(message=message,category=RuntimeWarning)
+            # Make chat strategy data False
+            chat_strategy_data = False
+        else:
+            # Read chat role attributes
+            try:
+                chat_role:dict = chat_strategy['role']
+            except KeyError:
+                raise StrategySectionMissingError('chat.role')
             else:
-                prompt = None
-        try:
-            input = role_content['input']
-        except KeyError:
-            raise StrategyParameterMissingError('chat.role','input')
-        try:
-            output = role_content['output']
-        except KeyError:
-            raise StrategyParameterMissingError('chat.role','output')
-        role = Role(prompt,input,output)
-        ## Read other chat strategy parameters
-        try:
-            stop = chat['stop']
-        except KeyError:
-            message = 'Missing `stop` parameter of `chat` section '
-            message += "in `strategy.toml` , auto-fallback to `[]`."
-            warn(message,RuntimeWarning)
-            stop = []
-        try:
-            temperature = chat['temperature']
-        except KeyError:
-            message = 'Missing `temperature` parameter of `chat` section '
-            message += "in `strategy.toml` , auto-fallback to `0`."
-            warn(message,RuntimeWarning)
-            temperature = 0
-        self.chat:Chat = Chat(role,addition,stop,temperature)
+                # Read chat role prompt
+                try:
+                    chat_role_prompt = chat_role['prompt']
+                except KeyError:
+                    # Make warning
+                    message = 'Missing `prompt` parameter in `chat.role` section '\
+                        'in strategy file.\n'\
+                        'Extra prompt not available(Crashing Exception).'
+                    warn(message=message,category=RuntimeWarning)
+                    # Make chat role prompt False
+                    chat_role_prompt = False
+                # Read chat role input
+                try:
+                    chat_role_input = chat_role['input']
+                except KeyError:
+                    raise StrategyParameterMissingError(section='chat.role',
+                                                        parameter='input')
+                # Read chat role output
+                try:
+                    chat_role_output = chat_role['output']
+                except KeyError:
+                    raise StrategyParameterMissingError(section='chat.role',
+                                                        parameter='output')
+                # Build `Role` dataclass
+                chat_role_data = Role(prompt=chat_role_prompt,
+                                      input=chat_role_input,
+                                      output=chat_role_output)
+            # Read chat prompt
+            try:
+                chat_prompt = chat_strategy['prompt']
+            except KeyError:
+                chat_prompt_str = False
+            else:
+                if chat_role_prompt != False:
+                    chat_prompt_path = Path(chat_prompt)
+                    try:
+                        chat_prompt_str:str = chat_prompt_path.read_text('UTF-8')
+                    except FileNotFoundError:
+                        raise FileNotFoundError('Prompt File Not Found.')
+                else:
+                    raise StrategyCrashError()
+            # Read chat stop
+            try:
+                chat_stop:str|list = chat_strategy['stop']
+            except KeyError:
+                raise StrategyParameterMissingError(section='chat',
+                                                    parameter='stop')
+            # Read chat temperature
+            try:
+                chat_temperature:float = chat_strategy['temperature']
+            except KeyError:
+                raise StrategyParameterMissingError(section='chat',
+                                                    parameter='temperature')
+            # Build `ChatStrategyData` dataclass
+            chat_strategy_data = ChatStrategyData(role=chat_role_data,
+                                                  prompt=chat_prompt_str,
+                                                  stop=chat_stop,
+                                                  temperature=chat_temperature)
+        # Build & Return `StrategyData` dataclass
+        if (call_strategy_data == False) and (chat_strategy_data == False):
+            raise StrategyNoAvailableModeError()
+        else:
+            return StrategyData(call=call_strategy_data,chat=chat_strategy_data)
 
     ## ============================== Update Methods ============================== ##
-    def update_call(self,stop:str|list,temperature:float) -> None:
-        '''The method is defined for update inference strategy for call.
+    def call(self,strategy:StrategyData,
+             stop:str|list=False,temperature:float=False) -> StrategyData:
+        '''The method will build new `StrategyData` instance 
+        with modified call inference strategy.
         Args:
-            stop: A string or a list of strings 
-                indicate where the model should stop generation.
-            temperature: A float indicate the model inference temperature.
+            strategy: A `StrategyData` instance indicates 
+                the original inference strategy.
+            stop: A `str` or `list` indicates the token(s) stopping generation.
+            temperature: A `float` indicates the temperature of model inference.
         '''
-        if stop != None:
-            self.call.stop = stop
-        if temperature != None:
-            self.call.temperature = temperature
+        # Verify strategy section initialization
+        if strategy.call == False:
+            raise StrategyOutOfRangeError('call')
+        # Catch changing parameters
+        if stop != False:
+            stop_value = stop
+        else:
+            stop_value = strategy.call.stop
+        if temperature != False:
+            temperature_value = temperature
+        else:
+            temperature_value = strategy.call.temperature
+        # Build & Reture new `StrategyData` dataclass
+        return StrategyData(call=CallStrategyData(stop=stop_value,
+                                                  temperature=temperature_value),
+                            chat=deepcopy(strategy.chat))
 
-    def update_chat(self,addition:str,
-                    prompt_role:str,input_role:str,output_role:str,
-                    stop:str|list,temperature:float) -> None:
-        '''The method is defined for update inference strategy for chat.
+    def chat(self,strategy:StrategyData,
+             prompt:str=False,
+             prompt_role:LlamaCppRole=False,
+             input_role:LlamaCppRole=False,output_role:LlamaCppRole=False,
+             stop:str|list=False,temperature:float=False) -> StrategyData:
+        '''The method will build new `StrategyData` instance 
+        with modified chat inference strategy.
         Args:
-            addition: A string indicate additional prompt for chat inference.
-            prompt_role: A string indicate the role of additional prompt.
-            input_role: A string indicate the role of input.
-            output_role: A string indicate the role of output.
-            stop: A string or a list of strings
-                indicate where the model should stop generation.
-            temperature: A float indicate the model inference temperature.
+            strategy: A `StrategyData` instance indicates 
+                the original inference strategy.
+            prompt: A `str` indicates the outside prompt for chat inference.
+            prompt_role: A given `str` indicates the role of outside prompt.
+            input_role: A given `str` indicates the role of input.
+            output_role: A given `str` indicate the role of output 
+                as known as the model response.
+            stop: A `str` or `list` indicates the token(s) stopping generation.
+            temperature: A `float` indicates the temperature of model inference.
         '''
-        if addition != None:
-            self.chat.addition = addition
-        if prompt_role != None:
-            if (not prompt_role) and (self.chat.addition):
-                error = "`prompt` parameter of `chat.role` setting can't be empty "
-                error += "when `addition` parameter of `chat` setting isn't empty. "
-                raise ValueError(error)
-            else:
-                self.chat.role.prompt = prompt_role
-        if input_role != None:
-            self.chat.role.input = input_role
-        if output_role != None:
-            self.chat.role.output = output_role
-        if stop != None:
-            self.chat.stop = stop
-        if temperature != None:
-            self.chat.temperature = temperature
-                    
+        # Verify strategy section initialization
+        if strategy.chat == False:
+            raise StrategyOutOfRangeError('chat')
+        # Catch changing parameters
+        if prompt != False:
+            prompt_value = prompt
+        else:
+            prompt_value = strategy.chat.prompt
+        if prompt_role != False:
+            role_prompt = prompt_role
+        else:
+            role_prompt = strategy.chat.role.prompt
+        if input_role != False:
+            role_input = input_role
+        else:
+            role_input = strategy.chat.role.input
+        if output_role != False:
+            role_output = output_role
+        else:
+            role_output = strategy.chat.role.output
+        if stop != False:
+            stop_value = stop
+        else:
+            stop_value = strategy.chat.stop
+        if temperature != False:
+            temperature_value = temperature
+        else:
+            temperature_value = strategy.chat.temperature
+        # Check compliance
+        if prompt_value and (role_prompt == False):
+            raise StrategyCrashError()
+        # Build & Return new `StrategyData` dataclass
+        return StrategyData(call=deepcopy(strategy.call),
+                            chat=ChatStrategyData(role=Role(prompt=role_prompt,
+                                                            input=role_input,
+                                                            output=role_output),
+                                                  prompt=prompt_value,
+                                                  stop=stop_value,
+                                                  temperature=temperature_value))
